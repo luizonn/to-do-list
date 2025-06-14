4db91abb5622036da08f13ba7f3307367053dd26
Felipe Luizon <luizonfelipe@gmail.com>
2025-06-13 22:40:02 -0300

    head: db connection

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://root:admin@todolist.9xw0kv1.mongodb.net/?retryWrites=true&w=majority&appName=todolist",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => console.log("MongoDB Atlar CONECTADO!"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectToDb;
